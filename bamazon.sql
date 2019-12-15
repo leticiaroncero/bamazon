ec16c04eeeca4e783bd8f18c5638f21cd62bc1c4
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2019-12-14 18:29:48 -0800

    update MySQL DECIMAL data type to store up to 2 decimals

--- bamazon.sql
+++ bamazon.sql
@@ -7,7 +7,7 @@ CREATE TABLE products (
     item_id INT NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(100) NOT NULL,
     department_name VARCHAR(100) NULL,
-    price DECIMAL(10, 4) NOT NULL, 
+    price DECIMAL(10, 2) NOT NULL, 
     stock_quantity INT NOT NULL,
     PRIMARY KEY (item_id)
 );
