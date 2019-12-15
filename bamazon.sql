2e6bd7342a900b7717b32ec2ccc85aa9f36edb63
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2019-12-14 18:24:29 -0800

    sql commands to populate database with products

--- bamazon.sql
+++ bamazon.sql
@@ -12,6 +12,15 @@ CREATE TABLE products (
     PRIMARY KEY (item_id)
 );
 
-
-
-
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES 
+    ("Kids Car & Toy Carpet", "Games", 24.95, 20),
+    ("Alexa Echo Dot Smart Speaker", "Electronics", 24.99, 50),
+    ("Fujifilm Instax Camera", "Electronics", 59.99, 30),
+    ("Donkey Kong Nintendo Switch Game", "Games", 54.95, 60),
+    ("The Great Gatsby (Classics)", "Books", 14.62, 10),
+    ("High Waist Yoga Pants", "Clothing", 89.00, 100),
+    ("Window Cleaner", "Gardening", 8.02, 120),
+    ("Crunchy Cat Treats", "Pet Supplies", 15.78, 160),
+    ("Waterproof Bluetooth Speaker", "Electronics", 79.95, 5),
+    ("Ugly Christmas Sweater", "Clothing", 29.99, 6);
