846bd8a6aec6137ce7415a7772a8f36d10de120b
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2019-12-16 15:34:25 -0800

    create bamazonManager app, connect to the db, and add prompt

---
+++ bamazonManager.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+var mysql = require("mysql");
+var Table = require('cli-table');
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.DB_PASSWORD,
+    database: "bamazon"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    showOptions();
+});
+
+function showOptions() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View Products for Sale",
+                "View Low Inventory",
+                "Add to Inventory",
+                "Add New Product"
+            ]
+        })
+        .then(function (answer) {
+            switch (answer.action) {
+                case "View Products for Sale":
+                    displayProducts();
+                    break;
+
+                case "View Low Inventory":
+                    displayLowInventory();
+                    break;
+
+                case "Add to Inventory":
+                    addToInventory();
+                    break;
+
+                case "Add New Product":
+                    addNewProduct();
+                    break;
+            }
+        });
+}
