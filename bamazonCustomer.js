b63643163f230057b5980a51fc8a70423ee92e58
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2019-12-17 22:18:23 -0800

    remove console logs

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,7 +13,6 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
     displayProducts();
 });
 
